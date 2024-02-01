f6d2217ac5326483772ac25c3e2940cc248ad6e5
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2024-02-01 22:51:01 +0900

    BROKERS: Crud Models

---
+++ VirtualBazaar.Core/Brokers/Storages/IStorageBroker.Users.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using VirtualBazaar.Core.Models.Foundations;
+
+namespace VirtualBazaar.Core.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<User> InsertUserAsync(User user);
+        ValueTask<User> UpdateUserAsync(User user);
+    }
+}
---
+++ VirtualBazaar.Core/Brokers/Storages/StorageBroker.Admin.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VirtualBazaar.Core.Models.Foundations;
+
+namespace VirtualBazaar.Core.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Admin> Admins { get; set; }
+
+        public async ValueTask<Admin> InsertAdminAsync(Admin admin) =>
+            await InsertAsync(admin);
+        
+        public async ValueTask<Admin> UpdateAdminAsync(Admin admin) =>
+            await UpdateAsync(admin);
+    }
+}
